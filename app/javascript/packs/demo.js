8aafdda028d5fbd23a7484723dab3160b06e51fd
Xiaopow <xiaoyang88@gmail.com>
2019-11-06 11:28:05 +0800

    fix files

--- app/javascript/packs/demo.js
+++ app/javascript/packs/demo.js
@@ -1,7 +1,1 @@
 import '@src/demo';
-
-<%= javascript_packs_with_chunks_tag 'demo', 'application' %>
-<%= stylesheet_packs_with_chunks_tag 'demo', 'application' %>
-<!-- Use the following when not using split chunks -->
-<!--<%= javascript_pack_tag 'demo' %>-->
-<!--<%= stylesheet_pack_tag 'demo' %>-->
